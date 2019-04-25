070ba220abe9542c2795177e5f7d13b7cc8804b6
igor <isrpo@yandex.ru>
2019-04-25 21:56:01 +0300

    fix cors

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors')
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // app.use(express.static(publicPath));
 
