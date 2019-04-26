4d02410bc578af20bd6bb96346d6280a37fd87d6
igor <isrpo@yandex.ru>
2019-04-26 12:33:30 +0300

    update front

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
   <div class="container-fluid mt-5" id="app">
     <div class="row">
       <div class="col-5 m-auto">
-<!--        <textarea @input="update"></textarea>-->
         <div class="input-group">
           <div class="input-group-prepend">
             <span class="input-group-text">Input text</span>
           </div>
-          <textarea class="form-control" aria-label="With textarea" rows="35" v-model="file.text"></textarea>
+          <textarea class="form-control" aria-label="With textarea" rows="35" v-model="file.text"
+                    @input="updateOut"></textarea>
         </div>
       </div>
       <div class="col-5 m-auto">
@@ -15,23 +15,20 @@
           <div class="input-group-prepend">
             <span class="input-group-text">Output text</span>
           </div>
-          <textarea class="form-control" aria-label="With textarea" rows="35"></textarea>
+          <textarea class="form-control" aria-label="With textarea" rows="35" v-model="output"></textarea>
         </div>
-<!--        <div v-html="compiledMarkdown"></div>-->
       </div>
       <div class="col-2 m-auto">
         <ul class="list-group">
-          <li class="list-group-item">Cras justo odio</li>
-          <li class="list-group-item">Dapibus ac facilisis in</li>
-          <li class="list-group-item">Morbi leo risus</li>
-          <li class="list-group-item">Porta ac consectetur ac</li>
-          <li class="list-group-item">Vestibulum at eros</li>
+          <li v-for="file in files" class="list-group-item">{{ file.date }}
+          <br><button class="btn btn-danger mr-2" @click="deleteFile">Delete</button>
+              <button class="btn btn-primary" @click="openFile">Open</button></li>
         </ul>
       </div>
     </div>
     <div class="row mt-2">
       <div class="col">
-        <button class="btn btn-success" @click="save">Save</button>
+        <button class="btn btn-success" @click="postFile">Save</button>
       </div>
     </div>
   </div>
@@ -45,24 +42,48 @@
         file: {
           text: '',
           date: new Date()
-        }
+        },
+        files: [],
+        output: ''
       }
     },
     created () {
       this.getList();
     },
     methods: {
-      save() {
-        const test = fetch('http://localhost:3000/api/files/')
+      getList() {
+        fetch('http://localhost:3000/api/files/')
+          .then(res => res.json())
+          .then(data => {
+            this.files = data;
+            console.log(this.files);
+          })
+          .catch(err => {
+            console.error(err);
+          });
+      },
+      postFile() {
+        fetch('http://localhost:3000/api/files/', {
+          method: 'POST',
+          body: JSON.stringify(this.file),
+          headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json'
+          }
+        })
           .then(res => res.json())
           .then(data => console.log(data))
-          .catch(err => {
-          console.error(err);
-        });
-        console.log(test);
+          .catch(err => console.error(err));
+        this.getList();
       },
-      getList() {
+      deleteFile() {
 
+      },
+      openFile() {
+
+      },
+      updateOut(e) {
+        this.output = e.target.value;
       }
     }
   }
@@ -76,7 +97,9 @@
 
 
 
-//   import axios from 'axios';
+
+
+  //   import axios from 'axios';
 //
 //   const url = 'http://localhost:3000/api/files/';
 //
